5582f7a2617dfd2ab29d7ff3d0b2ce23a811e25b
Gabriel Gustafsson <gabriel.gustafsson@student.ksgyf.se>
2026-03-18 11:19:37 +0100

    Lägger till index och unik nyckel

--- nhl_database.sql
+++ nhl_database.sql
@@ -1,8 +1,9 @@
+DROP DATABASE nhl_database;
 CREATE DATABASE nhl_database;
 USE nhl_database;
 
 CREATE TABLE player (
-    player_id INT AUTO_INCREMENT PRIMARY KEY,
+    player_id INT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     birth_date DATE NOT NULL,
@@ -13,14 +14,14 @@ CREATE TABLE player (
 );
 
 CREATE TABLE team (
-    team_id INT AUTO_INCREMENT PRIMARY KEY,
+    team_id INT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     city VARCHAR(100) NOT NULL,
     abbreviation VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE season (
-    season_id INT AUTO_INCREMENT PRIMARY KEY,
+    season_id INT PRIMARY KEY,
     label VARCHAR(20) NOT NULL,
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
@@ -28,7 +29,7 @@ CREATE TABLE season (
 );
 
 CREATE TABLE player_team_season (
-    player_team_season_id INT AUTO_INCREMENT PRIMARY KEY,
+    player_team_season_id INT PRIMARY KEY,
     player_id INT NOT NULL,
     team_id INT NOT NULL,
     season_id INT NOT NULL,
@@ -38,11 +39,12 @@ CREATE TABLE player_team_season (
     end_date DATE,
     FOREIGN KEY (player_id) REFERENCES player(player_id),
     FOREIGN KEY (team_id) REFERENCES team(team_id),
-    FOREIGN KEY (season_id) REFERENCES season(season_id)
+    FOREIGN KEY (season_id) REFERENCES season(season_id),
+    INDEX idx_pts_player_season (player_id, season_id)
 );
 
 CREATE TABLE game (
-    game_id INT AUTO_INCREMENT PRIMARY KEY,
+    game_id INT PRIMARY KEY,
     home_team_id INT NOT NULL,
     away_team_id INT NOT NULL,
     season_id INT NOT NULL,
@@ -55,11 +57,12 @@ CREATE TABLE game (
     shootout_flag BOOLEAN NOT NULL,
     FOREIGN KEY (home_team_id) REFERENCES team(team_id),
     FOREIGN KEY (away_team_id) REFERENCES team(team_id),
-    FOREIGN KEY (season_id) REFERENCES season(season_id)
+    FOREIGN KEY (season_id) REFERENCES season(season_id),
+    INDEX idx_game_season_date (season_id, game_date)
 );
 
 CREATE TABLE player_game_stats (
-    player_game_stats_id INT AUTO_INCREMENT PRIMARY KEY,
+    player_game_stats_id INT PRIMARY KEY,
     game_id INT NOT NULL,
     player_id INT NOT NULL,
     team_id INT NOT NULL,
@@ -73,11 +76,14 @@ CREATE TABLE player_game_stats (
     plus_minus INT NOT NULL,
     FOREIGN KEY (game_id) REFERENCES game(game_id),
     FOREIGN KEY (player_id) REFERENCES player(player_id),
-    FOREIGN KEY (team_id) REFERENCES team(team_id)
+    FOREIGN KEY (team_id) REFERENCES team(team_id),
+    UNIQUE KEY uq_player_game (game_id, player_id), -- en spelares statistik i en specifik match
+	INDEX idx_pgs_player_id (player_id),
+	INDEX idx_pgs_game_id (game_id)
 );
 
 CREATE TABLE team_game_stats (
-    team_game_stats_id INT AUTO_INCREMENT PRIMARY KEY,
+    team_game_stats_id INT PRIMARY KEY,
     team_id INT NOT NULL,
     game_id INT NOT NULL,
     shots INT NOT NULL,
@@ -87,5 +93,6 @@ CREATE TABLE team_game_stats (
     powerplay_goals INT NOT NULL,
     powerplay_opportunities INT NOT NULL,
     FOREIGN KEY (team_id) REFERENCES team(team_id),
-    FOREIGN KEY (game_id) REFERENCES game(game_id)
+    FOREIGN KEY (game_id) REFERENCES game(game_id),
+    UNIQUE KEY uq_team_game (team_id, game_id)
 );
